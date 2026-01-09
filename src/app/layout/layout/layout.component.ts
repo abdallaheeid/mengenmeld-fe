379ba2a5b492f2ed652adf7de20f973af774f581
Abdallah Eid <abdallaheeid@icloud.com>
2026-01-09 20:46:27 +0100

    Fix the footer to the bottom

--- src/app/layout/layout/layout.component.ts
+++ src/app/layout/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [FooterComponent, HeaderComponent],
+  imports: [FooterComponent, HeaderComponent, RouterModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
