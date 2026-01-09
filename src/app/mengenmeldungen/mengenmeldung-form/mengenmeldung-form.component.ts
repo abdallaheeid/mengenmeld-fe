09cf47f86ff2a7a5daa9f343b09540a6589df104
Abdallah Eid <abdallaheeid@icloud.com>
2026-01-09 17:52:28 +0100

    Added basic componenets and services

---
+++ src/app/mengenmeldungen/mengenmeldung-form/mengenmeldung-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mengenmeldung-form',
+  imports: [],
+  templateUrl: './mengenmeldung-form.component.html',
+  styleUrl: './mengenmeldung-form.component.css',
+})
+export class MengenmeldungFormComponent {
+
+}
---
+++ src/app/mengenmeldungen/mengenmeldung-list/mengenmeldung-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mengenmeldung-list',
+  imports: [],
+  templateUrl: './mengenmeldung-list.component.html',
+  styleUrl: './mengenmeldung-list.component.css',
+})
+export class MengenmeldungListComponent {
+
+}
