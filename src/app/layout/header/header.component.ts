19ea8aa9c3ef37c8affda11fb2bef5312eced25c
Abdallah Eid <abdallaheeid@icloud.com>
2026-01-09 18:56:58 +0100

    Adding basic navbar

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  constructor(public authService: AuthService) {}
+
+  logout() {
+    this.authService.logout();
+  }
 }
