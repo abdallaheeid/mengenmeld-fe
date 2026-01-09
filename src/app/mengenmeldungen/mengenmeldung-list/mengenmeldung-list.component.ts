a6c5a7fb55ade8daabb944b32ac2cdd802b6b798
Abdallah Eid <abdallaheeid@icloud.com>
2026-01-09 22:27:06 +0100

    Adding basic table with dummy examples and pagination and search field

--- src/app/mengenmeldungen/mengenmeldung-list/mengenmeldung-list.component.ts
+++ src/app/mengenmeldungen/mengenmeldung-list/mengenmeldung-list.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, input, InputOptionsWithTransform } from '@angular/core';
+import { NgbHighlight, NgbPagination } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-mengenmeldung-list',
-  imports: [],
+  imports: [NgbPagination],
   templateUrl: './mengenmeldung-list.component.html',
   styleUrl: './mengenmeldung-list.component.css',
 })
 export class MengenmeldungListComponent {
+  mengenmeldungen$: any;
+  service: any;
+  total$: any;
 
+  page = input.required<number>;
 }
