3590c6f9a9067ca68fc056bd9c5d6e5ee58ceccd
Abdallah Eid <abdallaheeid@icloud.com>
2026-01-09 18:00:40 +0100

    Adding basic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { MengenmeldungListComponent } from './mengenmeldungen/mengenmeldung-list/mengenmeldung-list.component';
+import { MengenmeldungFormComponent } from './mengenmeldungen/mengenmeldung-form/mengenmeldung-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'mengenmeldung', component: MengenmeldungListComponent },
+      { path: 'mengenmeldungen/new', component: MengenmeldungFormComponent },
+      { path: '', redirectTo: 'mengenmeldung', pathMatch: 'full' },
+    ],
+  },
+  { path: '**', redirectTo: 'login' },
+];
