e4a82bbb37806694cfe982c1b410c1182f8588b7
Abdallah Eid <abdallaheeid@icloud.com>
2026-01-11 00:31:37 +0100

    Adding app-root class to help with sticky note

--- src/app/layout/layout/layout.component.ts
+++ src/app/layout/layout/layout.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 import { RouterModule } from '@angular/router';
+import { Toast } from '../../helper/toast/toast-component';
 
 @Component({
   selector: 'app-layout',
-  imports: [FooterComponent, HeaderComponent, RouterModule],
+  imports: [FooterComponent, HeaderComponent, RouterModule, Toast],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
