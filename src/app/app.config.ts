23b319ed49fd49230d508bf325d348ad3022ac89
Abdallah Eid <abdallaheeid@icloud.com>
2026-01-10 13:59:35 +0100

    Registering interceptors and add adding jwt interceptor function

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,12 +8,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { authInterceptor } from './core/jwt-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([])),
+    provideHttpClient(withInterceptors([authInterceptor])),
   ],
 };
