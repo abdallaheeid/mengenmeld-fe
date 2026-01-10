cf3830ed26438e0ab54e3b50564fe23e73ae153e
Abdallah Eid <abdallaheeid@icloud.com>
2026-01-11 00:24:24 +0100

    Upading routes with lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,20 +3,29 @@ import { LoginComponent } from './auth/login/login.component';
 import { LayoutComponent } from './layout/layout/layout.component';
 import { MengenmeldungListComponent } from './mengenmeldungen/mengenmeldung-list/mengenmeldung-list.component';
 import { MengenmeldungFormComponent } from './mengenmeldungen/mengenmeldung-form/mengenmeldung-form.component';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
+  { path: 'login', component: LoginComponent },
+
   {
     path: '',
     component: LayoutComponent,
+    canActivate: [authGuard],
     children: [
-      { path: 'mengenmeldung', component: MengenmeldungListComponent },
-      { path: 'mengenmeldungen/new', component: MengenmeldungFormComponent },
-      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      {
+        path: 'mengenmeldungen',
+        loadComponent: () =>
+          import('./mengenmeldungen/mengenmeldung-list/mengenmeldung-list.component').then(
+            (m) => m.MengenmeldungListComponent
+          ),
+      },
+      {
+        path: 'mengenmeldungen/new',
+        component: MengenmeldungFormComponent,
+      },
     ],
   },
+
   { path: '**', redirectTo: 'login' },
 ];
