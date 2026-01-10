09126094be725c39db12ca765ea4a26ab28b7433
Abdallah Eid <abdallaheeid@icloud.com>
2026-01-11 00:32:20 +0100

    Login in mechasim

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { FormBuilder, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  errorMessage = signal<string | null>(null);
+
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
+  loginForm!: FormGroup;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.loginForm = this.fb.nonNullable.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+
+    this.loginForm.valueChanges.subscribe(() => {
+      if (this.errorMessage()) {
+        this.errorMessage.set(null);
+      }
+    });
+  }
+
+  login() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const { username, password } = this.loginForm.getRawValue();
+
+    this.authService.login(username, password).subscribe({
+      next: () => this.router.navigate(['/mengenmeldungen']),
+      error: () => this.errorMessage.set('Login fehlgeschlagen'),
+    });
+  }
+}
