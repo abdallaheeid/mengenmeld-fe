5b2e586da92de9dc58a56257c46243bdb968b11f
Abdallah Eid <abdallaheeid@icloud.com>
2026-01-11 00:30:21 +0100

    Getting the mengmenldung data from backend

---
+++ src/app/mengenmeldungen/einheit.modal.ts
@@ -0,0 +1,7 @@
+export interface Einheit {
+  STUECK: string;
+  KG: string;
+  TONNE: string;
+}
+
+// From backend  [STUECK, TONNE, KG]
--- src/app/mengenmeldungen/mengenmeldung-list/mengenmeldung-list.component.ts
+++ src/app/mengenmeldungen/mengenmeldung-list/mengenmeldung-list.component.ts
@@ -1,17 +1,27 @@
-import { Component, input, InputOptionsWithTransform } from '@angular/core';
+import { Component, inject, input, InputOptionsWithTransform, OnInit, signal } from '@angular/core';
 import { NgbHighlight, NgbPagination } from '@ng-bootstrap/ng-bootstrap';
 import { RouterLink } from '@angular/router';
+import { MengenmeldungService } from '../mengenmeldung.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mengenmeldung-list',
-  imports: [NgbPagination, RouterLink],
+  imports: [NgbPagination, RouterLink, CommonModule],
   templateUrl: './mengenmeldung-list.component.html',
   styleUrl: './mengenmeldung-list.component.css',
 })
-export class MengenmeldungListComponent {
-  mengenmeldungen$: any;
-  service: any;
+export class MengenmeldungListComponent implements OnInit {
+  mengenmeldungen = signal<any[]>([]);
   total$: any;
 
   page = input.required<number>;
+
+  private mengenMeldungService = inject(MengenmeldungService);
+
+  ngOnInit() {
+    this.mengenMeldungService.getAllMengenMeldungen().subscribe({
+      next: (data) => this.mengenmeldungen.set(data),
+      error: (err) => console.error('Failed to load Mengenmeldungen', err),
+    });
+  }
 }
