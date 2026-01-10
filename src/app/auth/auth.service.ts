887fb8e51ccf60efb1acaf3ff05cb12f28da2c8d
Abdallah Eid <abdallaheeid@icloud.com>
2026-01-10 13:47:04 +0100

    Minimal Auth service implementation

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
-
+import { inject, Inject, Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthService {
-  currentUsername() {}
-  logout() {}
+export class AuthService implements OnInit {
+  private readonly API_URL = 'http://localhost:8080/api/auth';
+  private httpClient = inject(HttpClient);
+
+  ngOnInit(): void {}
+
+  login(username: string, password: string) {
+    return this.httpClient
+      .post<{ token: string }>(`${this.API_URL}/login`, { username, password })
+      .pipe(
+        tap((response) => {
+          localStorage.setItem('token', response.token);
+        })
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
 }
