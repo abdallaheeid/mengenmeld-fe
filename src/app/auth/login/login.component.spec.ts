da6d305bc3d7dfa7a022182d92707a56179346fb
Abdallah Eid <abdallaheeid@icloud.com>
2026-01-15 17:28:04 +0100

    Testing the login functionality

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,23 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideZonelessChangeDetection } from '@angular/core';
 import { LoginComponent } from './login.component';
+import { AuthService } from '../auth.service';
+import { of } from 'rxjs';
+import { provideRouter } from '@angular/router';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authService: jasmine.SpyObj<AuthService>;
 
   beforeEach(async () => {
+    authService = jasmine.createSpyObj('AuthService', ['login']);
+
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
-    })
-    .compileComponents();
+      imports: [LoginComponent],
+      providers: [
+        provideRouter([]),
+        provideZonelessChangeDetection(),
+        {
+          provide: AuthService,
+          useValue: authService,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should call login on submit', () => {
+    authService.login.and.returnValue(of({ token: 'jwt' }));
+    component.loginForm.setValue({
+      username: 'admin',
+      password: 'admin123',
+    });
+
+    component.login();
+
+    expect(authService.login).toHaveBeenCalled();
   });
 });
