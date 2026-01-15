b7ae9a008444760b05ba236409b2ae1bb34551a1
Abdallah Eid <abdallaheeid@icloud.com>
2026-01-15 18:27:54 +0100

    Adding unit tests for mengenmeldung list

---
+++ src/app/mengenmeldungen/mengenmeldung-list/mengenmeldung-list.component.spec.ts
@@ -0,0 +1,75 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MengenmeldungListComponent } from './mengenmeldung-list.component';
+import { MengenmeldungService } from '../mengenmeldung.service';
+import { of } from 'rxjs';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+describe('MengenmeldungListComponent', () => {
+  let component: MengenmeldungListComponent;
+  let fixture: ComponentFixture<MengenmeldungListComponent>;
+  let service: jasmine.SpyObj<MengenmeldungService>;
+
+  const MOCK_DATA = [
+    {
+      id: 1,
+      zeitraum: '2025-03',
+      menge: 10,
+      einheit: 'KG',
+      geraeteartnummer: 123,
+      registrierungsnummer: 11111111,
+      status: 'SENT',
+    },
+    {
+      id: 2,
+      zeitraum: '2025-02',
+      menge: 5,
+      einheit: 'STUECK',
+      geraeteartnummer: 456,
+      registrierungsnummer: 22222222,
+      status: 'FAILED',
+    },
+  ];
+
+  beforeEach(async () => {
+    service = jasmine.createSpyObj('MengenmeldungService', ['getAllMengenMeldungen']);
+
+    service.getAllMengenMeldungen.and.returnValue(of(MOCK_DATA));
+
+    await TestBed.configureTestingModule({
+      imports: [MengenmeldungListComponent],
+      providers: [
+        provideRouter([]),
+        provideZonelessChangeDetection(),
+        { provide: MengenmeldungService, useValue: service },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MengenmeldungListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should load mengenmeldungen on init', () => {
+    expect(service.getAllMengenMeldungen).toHaveBeenCalled();
+    expect(component.mengenmeldungen().length).toBe(2);
+  });
+
+  it('should filter mengenmeldung by search term', () => {
+    component.searchTerm.set('KG');
+
+    const result = component.mengenmeldungen();
+
+    expect(result.length).toBe(1);
+    expect(result[0].einheit).toBe('KG');
+  });
+
+  it('should filter by status', () => {
+    component.searchTerm.set('failed');
+
+    const result = component.mengenmeldungen();
+
+    expect(result.length).toBe(1);
+    expect(result[0].status).toBe('FAILED');
+  });
+});
